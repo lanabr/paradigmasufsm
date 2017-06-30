e16f2a34a9b8a548f446edcd47370f3d23287bda
lanabr <lanarossato@hotmail.com>
2017-06-30 15:50:22 -0300

    Update compile.sh

--- t8/compile.sh
+++ t8/compile.sh
@@ -1,3 +1,3 @@
-g++ -std=c++11 `fltk-config --cxxflags` main.cpp trabalho.cxx `fltk-config --ldflags` 
+g++ -std=c++11 `fltk-config --cxxflags` main.cpp gerador.cxx `fltk-config --ldflags` 
 #Ou
-#g++ -std=c++11 main.cpp window.cxx -lfltk
+#g++ -std=c++11 main.cpp gerador.cxx -lfltk
